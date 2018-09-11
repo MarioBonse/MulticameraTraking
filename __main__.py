e845fae94057995763fa57dcb62a08534ec31cff
Mario Bonsembiante <mario.bonsembiante@gmail.com>
2018-09-11 16:07:47 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,17 +1,30 @@
 import camera as cam
+import HSVObjTracking as HSVTr
 import 3dplot
 
 def main():
     camera1 = cam.camera(1, "/dev/video0")
     camera2 = cam.camera(2, "/dev/video3")
+    #red ball
+    ballLower = (137, 88, 55)
+    ballUpper = (183, 255, 255)
     if camera1.createCameraMatrixUndistort() == False:
         return False
     if camera2.createCameraMatrixUndistort() == False:
         return False
     camera1.showvideo()
     camera2.showvideo()
-    x,y,z = camera1.TriangulateBallVideo(camera2, False)
+    trackingObject = HSVtr.HSVObjTracking(ballLower, ballUpper, camera1, camera2)
+    x,y,z = trackingObject.threedMovementsRecontstruction()
     3dplot.displayanimation(x, y, z)
 
 if __name__ == "__main__":
     main()
+
+###yellow ball
+#ballLower = (14, 67, 34)
+#ballUpper = (57, 255, 255)
+#
+######red ball
+#ballLower = (137, 88, 55)
+#ballUpper = (183, 255, 255)
